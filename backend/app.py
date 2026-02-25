72bf58e9c60819162c4aa1ba6999d1f02f11a056
Emily Sadler <emily@sadler.gg>
2026-02-25 13:44:50 +0000

    Project Setup

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+@app.route("/")
+def home():
+    return jsonify({"message": "Hangman API running!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
